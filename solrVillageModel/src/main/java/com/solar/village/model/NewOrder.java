6afc1cbf015fb8b94397317d73c3db9a28043f06
nreidelb <nreidelb@redhat.com>
2016-06-09 16:04:55 -0400

    initial work

---
+++ solrVillageModel/src/main/java/com/solar/village/model/NewOrder.java
@@ -0,0 +1,12 @@
+package com.solar.village.model;
+
+import java.io.Serializable;
+
+public class NewOrder implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6395274638700451893L;
+
+}
